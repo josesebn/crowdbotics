2088e71b8f9e34951ae978a7e6ea1b18e782f8b4
josesebn <josesebn@gmail.com>
2025-02-18 17:15:42 -0500

    aks changes

--- terraform/main.tf
+++ terraform/main.tf
@@ -20,6 +20,14 @@
    }
  }
 
+ provider "kubernetes" {
+  host = azurerm_kubernetes_cluster.aks.kube_config.0.host
+  client_certificate = base64decode(azurerm_kubernetes_cluster.aks.kube_config.0.client_certificate)
+  client_key = base64decode(azurerm_kubernetes_cluster.aks.kube_config.0.client_key)
+  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks.kube_config.0.cluster_ca_certificate)
+}
+
+
 resource "kubernetes_deployment" "main" {
   depends_on = [azurerm_kubernetes_cluster.myaks]
 
@@ -82,3 +90,4 @@ resource "kubernetes_service" "main" {
     type = "LoadBalancer"
   }
 }
+
